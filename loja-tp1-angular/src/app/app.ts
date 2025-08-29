3d2465270c0efd427b28ffe587f0ba2ae51145d9
renata-aluna <renata.l@aluno.ifsp.edu.br>
2025-08-29 22:56:05 +0000

    AULA ANTERIOR

--- loja-tp1-angular/src/app/app.ts
+++ loja-tp1-angular/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { Header } from './core/header/header';
+import { Footer } from './core/footer/footer';
+import { Banner } from './core/banner/banner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, Banner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
